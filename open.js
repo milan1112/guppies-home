1fffade97986ce4f73d28925cc67debc1000bb29
milanjiji <milanjiji0987654321@gmail.com>
2020-11-15 15:56:04 +0530

    guppies-home

---
+++ open.js
@@ -0,0 +1,10 @@
+function openfunction(){
+    document.getElementById('menu').style.width="50%"
+    
+  }
+  function closefunction(){
+    document.getElementById('menu').style.width="0%"
+    
+  }
+
+  
